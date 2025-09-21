b97ef33df97c9690b9b5660d067b64d0691563ee
Valerii Shymchuk <valera01092006@gmail.com>
2025-09-21 10:29:59 +0300

    Improved grid

--- composeApp/src/jvmMain/kotlin/ua/valeriishymchuk/lobmapeditor/render/stage/GridStage.kt
+++ composeApp/src/jvmMain/kotlin/ua/valeriishymchuk/lobmapeditor/render/stage/GridStage.kt
@@ -2,10 +2,12 @@ package ua.valeriishymchuk.lobmapeditor.render.stage
 
 import com.jogamp.opengl.GL3
 import org.joml.Matrix4f
+import org.joml.Vector3f
 import ua.valeriishymchuk.lobmapeditor.render.context.RenderContext
 import ua.valeriishymchuk.lobmapeditor.render.helper.glBindVBO
 import ua.valeriishymchuk.lobmapeditor.render.program.GridProgram
 import ua.valeriishymchuk.lobmapeditor.render.resource.ResourceLoader.loadShaderSource
+import kotlin.math.max
 
 class GridStage(
     ctx: GL3,
@@ -29,10 +31,13 @@ class GridStage(
         program.setUpVBO(glCtx, mapVertices)
         program.setUpVAO(glCtx)
 
+        val thicknessScale = max((2f / viewMatrix.getScale(Vector3f()).x), 1f)
+
+
         program.applyUniform(glCtx, GridProgram.Uniform(
             gridContext.offset,
             gridContext.gridSize,
-            gridContext.gridThickness,
+            gridContext.gridThickness * thicknessScale,
             gridContext.color,
             mvpMatrix
         ))
