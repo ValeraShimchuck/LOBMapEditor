4de5deea88c877281cdeb7bbca9f93ccb595f8ec
Valerii Shymchuk <valera01092006@gmail.com>
2025-09-23 00:24:10 +0300

    Tried to improve grid render, but failed, nothing changed actually

--- composeApp/src/jvmMain/kotlin/ua/valeriishymchuk/lobmapeditor/render/stage/GridStage.kt
+++ composeApp/src/jvmMain/kotlin/ua/valeriishymchuk/lobmapeditor/render/stage/GridStage.kt
@@ -30,14 +30,15 @@ class GridStage(
 
         program.setUpVBO(glCtx, mapVertices)
         program.setUpVAO(glCtx)
+        val zoomScale = viewMatrix.getScale(Vector3f()).x
 
-        val thicknessScale = max(((1f / gridContext.gridThickness) / viewMatrix.getScale(Vector3f()).x), 1f)
+        val thicknessScale = max(((1f / gridContext.gridThickness) / zoomScale), 1f) // old
 
 
         program.applyUniform(glCtx, GridProgram.Uniform(
             gridContext.offset,
             gridContext.gridSize,
-            gridContext.gridThickness * thicknessScale,
+            gridContext.gridThickness * thicknessScale, // used here
             gridContext.color,
             mvpMatrix
         ))
